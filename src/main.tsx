e45a73c20162b85abc7b08a328278a04eae7a4a9
DevLuong <luong1tan1gia123@gmail.com>
2025-05-07 15:36:15 +0700

    123

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,45 @@
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { message } from 'antd'
+
+import RootLayout from '@layouts/RootLayout'
+import ProtectedLayout from '@layouts/ProtectedLayout'
+import AuthLayout from '@layouts/AuthLayout'
+
+import HomePage from '@pages/HomePage'
+import LoginPage from '@pages/auth/LoginPage'
+
 import dayjs from 'dayjs'
 import 'dayjs/locale/vi'
 import updateLocale from 'dayjs/plugin/updateLocale'
-import './index.css'
-import { message } from 'antd'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import 'dayjs/locale/vi' // Import ngôn ngữ tiếng Việt
 import localizedFormat from 'dayjs/plugin/localizedFormat'
-import RootLayout from '@layouts/RootLayout'
-import ProtectedLayout from '@layouts/ProtectedLayout'
-import HomePage from '@pages/HomePage'
-import AuthLayout from '@layouts/AuthLayout'
-import LoginPage from '@pages/auth/LoginPage'
+
+import './index.css'
 import '@ant-design/v5-patch-for-react-19'
+
+// DayJS setup
+dayjs.extend(updateLocale)
+dayjs.extend(localizedFormat)
+dayjs.updateLocale('vi', { weekStart: 0 })
+dayjs.locale('vi')
+
+// QueryClient setup
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      retryDelay: 1000,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+      staleTime: 1000 * 60 * 5
+    }
+  }
+})
+
+// Router
 const router = createBrowserRouter([
   {
     path: '/',
@@ -22,45 +47,21 @@ const router = createBrowserRouter([
     children: [
       {
         element: <ProtectedLayout />,
-        children: [
-          {
-            path: '/',
-            element: <HomePage />
-          }
-        ]
+        children: [{ path: '/', element: <HomePage /> }]
       },
-
       {
         element: <AuthLayout />,
-        children: [
-          {
-            path: '/login',
-            element: <LoginPage />
-          }
-        ]
-      }
+        children: [{ path: '/login', element: <LoginPage /> }]
+      },
+      { path: '*', element: <p>NotFoundPage</p> } // 👈 Route 404 fallback
     ]
   }
 ])
-dayjs.extend(updateLocale)
-dayjs.updateLocale('vi', {
-  weekStart: 0
-})
-dayjs.extend(localizedFormat)
-dayjs.locale('vi')
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 5,
-      retryDelay: 1000,
-      refetchOnWindowFocus: false, // 👈 Không gọi lại khi chuyển tab
-      refetchOnReconnect: false, // 👈 Không gọi lại khi reconnect mạng
-      refetchOnMount: false, // 👈 Không gọi lại khi component mount                    // 👈 Retry tối đa 1 lần (tuỳ mày)
-      staleTime: 1000 * 60 * 5
-    } // 👈 Cache sống 5 phút (tuỳ mày) } }
-  }
-})
+
+// AntD global config
 message.config({ duration: 3, maxCount: 1 })
+
+// Render app
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
